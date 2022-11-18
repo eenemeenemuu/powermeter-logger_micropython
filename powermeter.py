f9b893b2b379f3b3c52f424767627494c1ca5df0
eenemeenemuu <eenemeenemuu@googlemail.com>
2022-11-18 09:27:44 +0100

    Added optional 'power_threshold' config value (minimum required power to log power value, may be required if micro inverter has measurable consumption at night).

--- powermeter.py
+++ powermeter.py
@@ -1,12 +1,13 @@
 powermeter_cfg = """
 # structure of config file 'powermeter_cfg.py'
-device = ''         # possible options: fritzbox, shelly
-host = ''           # host name or ip address of power meter, e.g. fritz.box or 192.168.178.1
-username = ''       # fritzbox only: user name
-fritz_pw = ''       # fritzbox only: password
-ain = ''            # fritzbox only: Aktor Identifikationsnummer (AIN), on your FRITZ!Box go to "Heimnetz > Smart Home" and edit your device to get the AIN
-host_external = ''  # external host for logging and display - must end with trailing slash (/)
-host_auth_key = ''  # auth key must match on external host
+device = ''             # possible options: fritzbox, shelly
+host = ''               # host name or ip address of power meter, e.g. fritz.box or 192.168.178.1
+username = ''           # fritzbox only: user name
+fritz_pw = ''           # fritzbox only: password
+ain = ''                # fritzbox only: Aktor Identifikationsnummer (AIN), on your FRITZ!Box go to "Heimnetz > Smart Home" and edit your device to get the AIN
+host_external = ''      # external host for logging and display - must end with trailing slash (/)
+host_auth_key = ''      # auth key must match on external host
+power_threshold = 0     # optional: minimum required power to log power value (may be required if micro inverter has measurable consumption at night); can be float value like 0.25
 """
 
 try:
@@ -60,7 +61,7 @@ def powermeter_stats():
                 t = cettime()
                 stats_date = '{:02d}.{:02d}.{:04d}'.format(t[2], t[1], t[0])
                 stats_time = ',{:02d}:{:02d}:{:02d}'.format(t[3], t[4], t[5])
-                stats_power = ',{:.2f}'.format(int(xml_get(stats, 'power')) / 100)
+                stats_power = ',{:.2f}'.format(power_threshold_get(int(xml_get(stats, 'power')) / 100))
                 stats_temp = ',{:.1f}'.format(int(xml_get(stats, 'temperature')) / 10)
 
             elif (device == "shelly"):
@@ -80,7 +81,7 @@ def powermeter_stats():
 
                 stats_date = '{:02d}.{:02d}.{:04d}'.format(t[2], t[1], t[0])
                 stats_time = ',{:02d}:{:02d}:{:02d}'.format(t[3], t[4], t[5])
-                stats_power = ',{:.2f}'.format(float(stats['meters'][0]['power']))
+                stats_power = ',{:.2f}'.format(power_threshold_get(float(stats['meters'][0]['power'])))
                 if 'temperature' in stats:
                     stats_temp = ',{:.2f}'.format(float(stats['temperature']))
                 else:
@@ -140,6 +141,16 @@ def utf8_to_utf16(text):
     for x in text:
         text_utf16 += x + '\x00'
     return text_utf16
+
+def power_threshold_get(power):
+    try:
+        power_threshold
+    except:
+        return power
+    if (power < power_threshold):
+        return 0
+    else:
+        return power
 
 def sync_time(pwm):
     pwm.freq(10)
