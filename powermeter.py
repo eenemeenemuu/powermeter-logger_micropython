fbc603e194ed376236308fe36598c489357f4828
eenemeenemuu <eenemeenemuu@googlemail.com>
2023-03-24 12:15:13 +0100

    Added support for Tasmota devices.

--- powermeter.py
+++ powermeter.py
@@ -26,7 +26,7 @@ except ImportError:
 def powermeter_stats():
     pwm.freq(1)
 
-    if (device == "shelly"):
+    if (device == "shelly" or device == "tasmota"):
         import json
     import time
     import gc
@@ -89,6 +89,21 @@ def powermeter_stats():
                     stats_temp = ',{:.2f}'.format(float(stats['temperature']))
                 else:
                     stats_temp = ''
+
+            elif (device == "tasmota"):
+                stats = http_get('http://'+host+'/cm?cmnd=Status%208')
+
+                if (stats == ""):
+                    print('Failed')
+                    continue
+
+                stats = json.loads(stats[stats.find('{'):])
+                t = stats['StatusSNS']['Time']
+
+                stats_date = '{:02d}.{:02d}.{:04d}'.format(int(t[8:10]), int(t[5:7]), int(t[0:4]))
+                stats_time = ',{:02d}:{:02d}:{:02d}'.format(int(t[11:13]), int(t[14:16]), int(t[17:19]))
+                stats_power = ',{:.2f}'.format(power_threshold_get(float(stats['StatusSNS']['ENERGY']['Power'])))
+                stats_temp = ''
 
             else:
                 print('wrong device configured')
