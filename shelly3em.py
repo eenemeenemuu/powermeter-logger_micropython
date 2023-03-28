482e3f26c303ef528a111781f744b5ba33e842b3
eenemeenemuu <eenemeenemuu@googlemail.com>
2023-03-28 17:26:28 +0200

    Added individual phases to Shelly 3EM log.

--- shelly3em.py
+++ shelly3em.py
@@ -24,5 +24,8 @@ def get_stats():
     stats_date = '{:02d}.{:02d}.{:04d}'.format(t[2], t[1], t[0])
     stats_time = ',{:02d}:{:02d}:{:02d}'.format(t[3], t[4], t[5])
     stats_power = ',{:.2f}'.format(power_threshold_get(float(stats['total_power'])))
+    stats_emeters = ','
+    for emeter in stats['emeters']:
+        stats_emeters += ',' + str(emeter['power'])
 
-    return stats_date + stats_time + stats_power
+    return stats_date + stats_time + stats_power + stats_emeters
