237476ee7351328496c29f2921a2b30aade400c5
eenemeenemuu <eenemeenemuu@googlemail.com>
2023-05-03 20:50:45 +0200

    More precise power value for Tasmota devices.

--- tasmota.py
+++ tasmota.py
@@ -16,6 +16,6 @@ def get_stats():
 
     stats_date = '{:02d}.{:02d}.{:04d}'.format(int(t[8:10]), int(t[5:7]), int(t[0:4]))
     stats_time = ',{:02d}:{:02d}:{:02d}'.format(int(t[11:13]), int(t[14:16]), int(t[17:19]))
-    stats_power = ',{:.2f}'.format(power_threshold_get(float(stats['StatusSNS']['ENERGY']['Power'])))
+    stats_power = ',{:.2f}'.format(power_threshold_get(float(stats['StatusSNS']['ENERGY']['Voltage'])*float(stats['StatusSNS']['ENERGY']['Current'])*float(stats['StatusSNS']['ENERGY']['Factor'])))
 
     return stats_date + stats_time + stats_power
